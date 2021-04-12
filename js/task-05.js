6094ae12ca0af7efa7a105f2b72ce19e4c7ca3d0
serhii.fedorenko90@gmail.com <serhii.fedorenko90@gmail.com>
2021-04-12 09:55:47 +0300

    5th done

--- js/task-05.js
+++ js/task-05.js
@@ -1,14 +1,9 @@
 const refs = {
   nameLabel: document.querySelector("#name-input"),
-  greetings: document.querySelector("#name-output"),
+  greeting: document.querySelector("#name-output"),
 };
-
 refs.nameLabel.addEventListener("input", onInputChange);
-
 function onInputChange(event) {
-  if (refs.nameLabel.textContent !== " ") {
-      refs.greetings.textContent = event.currentTarget.value;
-} else {
-    refs.greetings.textContent = "незнакомец";
-  }
+  let name = event.currentTarget.value;
+  refs.greeting.textContent = name.length !== 0 ? name : "незнакомец";
 }
